4f1f6b587cbdc32999a27f3348a36f5e8481fbf9
Stanislav.Petr <stanislav.petr7@gmail.com>
2024-02-15 17:10:10 +0100

    Added models

---
+++ src/main/java/dev/stan05/moviesProject/Movie.java
@@ -0,0 +1,30 @@
+package dev.stan05.moviesProject;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data // adds getters and setters to all properties
+@AllArgsConstructor // creates auto constructor to add all properties
+@NoArgsConstructor // another constructor without params
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
+}
